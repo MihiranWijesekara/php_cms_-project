8a9d5708df5bc3655de2928d9a65c285865ba46c
MihiranWijesekara <achinthamihiran654@gamil.com>
2024-04-25 01:46:41 +0530

    Your commit message here

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -71,7 +71,6 @@ if (isset($_POST['create_post'])) {
     <div class="form-group">
         <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
     </div>
-
+    <!----Achintha Wijesekara--->
 </form>
 
-<!----Achintha Wijesekara--->
