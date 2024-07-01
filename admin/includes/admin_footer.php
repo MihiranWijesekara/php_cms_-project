43a5e89964276b955d680db0a863ae0ba9e0b879
MihiranWijesekara <achinthamihiran654@gamil.com>
2024-07-02 03:22:02 +0530

    Your commit message here

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -7,7 +7,8 @@
  <!-- /#wrapper -->
 
  <!-- jQuery -->
- <script src="js/jquery.js"></script>
+ <script src="js/jquery.js"></script> 
+ <script src="js/script.js"></script>
 
  <!-- Bootstrap Core JavaScript -->
  <script src="js/bootstrap.min.js"></script>
---
+++ admin/js/script.js
@@ -0,0 +1,15 @@
+tynymce.init({selector:'textarea'});
+
+$(document).ready(function(){
+    $('#selectAllBoxes').click(function(event){
+        if(this.checked){
+            $('.checkBoxes').each(function(){
+                this.checked = true;
+            });
+        } else {
+            $('.checkBoxes').each(function(){
+                this.checked = false;
+            });
+        }
+    });
+});
